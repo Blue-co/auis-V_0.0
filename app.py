c58166a2bed0e9dd9eb50320cc9881cc53e7e392
Blue-co <aa39017867@gmail.com>
2025-12-29 21:29:36 +0900

    Specify static URL path in Flask app
    Updated Flask app initialization to specify static URL path.

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
 from flask import Flask, render_template
 
-app = Flask(__name__)
+# app.py
+app = Flask(__name__, static_url_path='/static')
 
 # 환경 변수에서 비밀키와 포트 가져오기
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'dev_key_123')
