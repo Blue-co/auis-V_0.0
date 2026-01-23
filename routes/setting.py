6de29d61a033a536efe85e9415166f146db7e983
Blue-co <aa39017867@gmail.com>
2026-01-23 11:06:35 +0900

    Create setting.py

---
+++ routes/setting.py
@@ -0,0 +1,7 @@
+from flask import Blueprint, render_template
+
+shopping_bp = Blueprint('setting', __name__)
+
+@shopping_bp.route('/setting')
+def shopping_list():
+    return render_template('shopping.html')
