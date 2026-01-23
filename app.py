f72089503036e695a990dc7995cb73f57e3128bd
Blue-co <aa39017867@gmail.com>
2026-01-23 13:04:33 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 from flask import Flask
 # 아까 만든 블루프린트를 불러옵니다.
-from routes.home import home_bp
 from routes.auiss import auiss_bp
 from routes.setting import setting_bp
 
 app = Flask(__name__)
 
 # 블루프린트 등록 (이게 핵심입니다!)
-app.register_blueprint(home_bp)
 app.register_blueprint(auiss_bp)
 app.register_blueprint(setting_bp)
 
